f2ea2a96551c73a6cbd3c993ee918d7e19453e8b
JhonDev <jhonatanjf0408@gmail.com>
2024-03-27 20:52:37 -0300

    cadastro

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('#telefone').mask('(00) 0000-0000');
+$('#cpf').mask('000.000.000-00');
+$('#cep').mask('00000-000');
+
+$('#cadastro-form').submit(function(event) {
+    event.preventDefault();
+    // Aqui você pode adicionar o código para lidar com o envio do formulário
+});
